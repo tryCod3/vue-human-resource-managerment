877e5bc828b9461f51723ba49617899f5152add6
tryCod3 <nthuy1999kadd1@gmail.com>
2022-09-23 15:01:40 +0700

    feat(employee): create list employee

---
+++ src/views/employee/module.ts
@@ -0,0 +1,34 @@
+import { IDepartmentState } from '@/views/department/module';
+import { ICompanyState } from '@/views/company/module';
+
+export interface IModelIdAndName {
+  id: number;
+  name: string;
+}
+
+export interface IEmployeeState {
+  id: number;
+  code: string;
+  email: string;
+  encrypted_password: string;
+  full_name: string;
+  gender: string;
+  birtday: string;
+  adress: string;
+  department: IDepartmentState;
+  hometown: string;
+  phone_number: string;
+  onboard_date: string;
+  position: IModelIdAndName;
+  end_date: string;
+  marital_status: boolean;
+  companies: ICompanyState[];
+  level: IModelIdAndName;
+  contract: IModelIdAndName;
+  role: IModelIdAndName;
+  degree: string;
+  skill: string;
+  avatar: string;
+  active: boolean;
+  citizen_identification: number;
+}
